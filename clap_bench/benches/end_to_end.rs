67df2bea258f3e6054adf827f0397308ae45e838
sineptic <sineptic0@gmail.com>
2025-07-24 14:32:26 +0300

    move benching functions to lib

--- clap_bench/benches/end_to_end.rs
+++ clap_bench/benches/end_to_end.rs
@@ -1,56 +1,23 @@
-use std::{hint::black_box, time::Duration};
+use std::time::Duration;
 
 use clap_bench::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn build(c: &mut Criterion) {
     c.bench_function("build args", |b| {
-        b.iter(|| {
-            complex::create_app();
-            empty::create_app();
-            ripgrep::app_short();
-            ripgrep::app_long();
-            simple::create_app();
-            rustup::build_cli();
-        });
+        b.iter(build_bench);
     });
 }
 
 fn startup(c: &mut Criterion) {
-    c.bench_function("startup", |b| {
-        b.iter(|| {
-            for args in complex::ARGS {
-                complex::create_app().get_matches_from(black_box(*args));
-            }
-            for args in empty::ARGS {
-                empty::create_app().get_matches_from(black_box(*args));
-            }
-            for args in ripgrep::ARGS {
-                ripgrep::app_short().get_matches_from(black_box(*args));
-            }
-            for args in ripgrep::ARGS {
-                ripgrep::app_long().get_matches_from(black_box(*args));
-            }
-            for args in simple::ARGS {
-                simple::create_app().get_matches_from(black_box(*args));
-            }
-            for args in rustup::ARGS {
-                rustup::build_cli().get_matches_from(black_box(*args));
-            }
-        });
+    c.bench_function("parse", |b| {
+        b.iter(parse_bench);
     });
 }
 
 fn render_help(c: &mut Criterion) {
     c.bench_function("render help", |b| {
-        b.iter(|| {
-            complex::create_app().render_help().to_string();
-            empty::create_app().render_help().to_string();
-            ripgrep::app_long().render_help().to_string();
-            ripgrep::app_short().render_help().to_string();
-            simple::create_app().render_help().to_string();
-            rustup::build_cli().render_help().to_string();
-        });
+        b.iter(render_help_bench);
     });
 }
 
--- clap_bench/src/lib.rs
+++ clap_bench/src/lib.rs
@@ -1,5 +1,44 @@
+use std::hint::black_box;
+
 pub mod complex;
 pub mod empty;
 pub mod ripgrep;
 pub mod rustup;
 pub mod simple;
+
+pub fn build_bench() {
+    complex::create_app();
+    empty::create_app();
+    ripgrep::app_short();
+    ripgrep::app_long();
+    simple::create_app();
+    rustup::build_cli();
+}
+pub fn parse_bench() {
+    for args in complex::ARGS {
+        complex::create_app().get_matches_from(black_box(*args));
+    }
+    for args in empty::ARGS {
+        empty::create_app().get_matches_from(black_box(*args));
+    }
+    for args in ripgrep::ARGS {
+        ripgrep::app_short().get_matches_from(black_box(*args));
+    }
+    for args in ripgrep::ARGS {
+        ripgrep::app_long().get_matches_from(black_box(*args));
+    }
+    for args in simple::ARGS {
+        simple::create_app().get_matches_from(black_box(*args));
+    }
+    for args in rustup::ARGS {
+        rustup::build_cli().get_matches_from(black_box(*args));
+    }
+}
+pub fn render_help_bench() {
+    complex::create_app().render_help().to_string();
+    empty::create_app().render_help().to_string();
+    ripgrep::app_long().render_help().to_string();
+    ripgrep::app_short().render_help().to_string();
+    simple::create_app().render_help().to_string();
+    rustup::build_cli().render_help().to_string();
+}
